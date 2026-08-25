7a659fffa9e564890e947cf803dbfcc30ec10775
devshadow14 <nd172237@gmail.com>
2026-08-25 12:22:42 +0000

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    self: false,          // false = bot public, true = bot privé (répond seulement au owner)
+    prefix: '.',
+    botName: 'CHAT KOIR MD',
+    ownerNumber: process.env.OWNER_NUMBER || ''
+}
